0f316c7c5baa2a8a875f484a830df5b43bef61c2
mani <mani>
2022-12-13 08:35:36 +0530

    load

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,28 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static class Product{
         public int product(int x,int y){
-            return x+y;
+            return x*y;
         }
         public int product(int x,int y,int z){
-            return x+y+z;
+            return x*y*z;
         }
         public double product(double x,double y){
-            return x+y;
+            return x*y;
         }
     }
     public static void main(String[] args){
         Product p=new Product();
-        p.product(2,3);
-        p.product(2,3,4);
-        p.product(.0,3.0);
+        int x=p.product(2,3);
+
+       int y= p.product(2,3,4);
+        double z=p.product(2.0,3.0);
+        System.out.println(x+" "+y+" "+z);
+
     }
 
 }
